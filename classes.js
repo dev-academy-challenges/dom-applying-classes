44d0cb404f3a23ed2e6b19a435ac310386caf61e
ursula <frickeursu@icloud.com>
2016-09-15 23:11:05 +1200

    adds function makeVisible

--- classes.js
+++ classes.js
@@ -8,6 +8,7 @@ function start () {
   one()
   two()
   three()
+  makeVisible()
 
   // Your turn! Create a new function called `two`, then call it from here.
 }
@@ -30,5 +31,8 @@ function three() {
   three.classList.add('pink')
 }
 
-
-// CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+makeVisible.classList.remove('invisible')[0]
+makeVisible.classList.add('visible')[0]
+}
